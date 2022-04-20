0e959f0f6a047244b4260d9b94a4c5b56809c016
Matteo Piazzi | AQuest <matteo.piazzi@aquest.it>
2022-04-20 17:31:47 +0200

    add project NUnit

---
+++ AquardensNUnitTest/wsCommerce002.cs
@@ -0,0 +1,31 @@
+﻿using AquardensNUnitTest;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace wsCommerce
+{
+    public class Report002 : Base001
+    {
+        protected iReport_002Client ReportClient = new iReport_002Client();
+        
+        [Test]
+        public async Task GetAquisti()
+        {
+            var baseRequest = new dcBaseRequest()
+            {
+                Impianto = Impianto,
+                SessionId = SessionId
+            };
+
+            var esito = await ReportClient.GetAcquistiAsync(baseRequest, new dcAcquistoFilter());
+            Print(esito);
+            Assert.IsNotNull(esito);
+        }
+        
+        [OneTimeTearDown]
+        public void TearDownClient()
+        {
+            ReportClient.Close();
+        }    
+    }
+}
